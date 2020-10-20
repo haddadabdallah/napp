cf784d2010a14080883e466d80c7961609734b88
Abdallah Haddad <sidibelabbes23@gmail.com>
2020-10-20 20:05:53 +0100

    add time

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,44 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 
-export default function Home() {
+  const getdata = async () =>{
+
+
+    const res = await fetch('https://api.pray.zone/v2/times/today.json?city=paris').then(
+      
+    (res) =>  res.json()
+    
+    )
+    return res
+  }
+
+  export  const getStaticProps  = async ({params}) =>{
+
+    const adan = await getdata()
+
+    return {
+      props : {adan}
+    }
+
+    
+  }
+
+
+
+export default function Home(props) {
+
+
+  console.log(props.adan.results.datetime[0].times.Imsak);
+
   return (
     <div className='container'>
-      
+
+  <h1>{props.adan.results.datetime[0].times.Imsak}</h1>
+
         <h1> store app </h1>
         <Link href="/produits">
+
+
         <button type="button" class="btn btn-primary">Primary</button>
         </Link>
 
--- pages/produits.js
+++ pages/produits.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function produits() {
     return (
-        <div>
+        <div className="container" >
             
             <h1>page de produit</h1>
 
